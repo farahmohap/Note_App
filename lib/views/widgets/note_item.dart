9b87f46747a534481b8b02bf5d85ae0c6b48e6b0
farah <farahmohap111@gmail.com>
2023-04-20 06:46:20 +0200

    C

---
+++ lib/views/widgets/note_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top:24,bottom: 24,left: 16, ),
+      decoration: BoxDecoration(
+          color: Colors.amber, borderRadius: BorderRadius.circular(16)),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+        ListTile(
+          title: Text(
+            "Flutter Tips",
+            style: TextStyle(color: Colors.black, fontSize: 26),
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(top: 16.0,bottom: 16),
+            child: Text("Build Your Career",
+                style:
+                    TextStyle(color: Colors.black.withOpacity(.5), fontSize: 18)),
+          ),
+          trailing: IconButton(
+            onPressed: (() {}),
+            icon: Icon(
+              FontAwesomeIcons.trash,
+              size: 24,
+               color: Colors.black),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right:24 ),
+          child: Text("May21, 2022",
+              style: TextStyle(fontSize:16,color: Colors.black.withOpacity(.4))),
+        )
+      ]),
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:noteapp/views/widgets/custom_app_bar.dart';
+import 'package:noteapp/views/widgets/note_item.dart';
+import 'package:noteapp/views/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -10,11 +12,16 @@ class NotesViewBody extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24),
         child: Column(
-          children: const [
+          children:const  [
             CustomAppBar(),
+            NoteItem(),
+            Expanded(child: NotesListView())
+
+            
           ],
         ),
       ),
     );
   }
 }
+
